32bef8ef6670ce0d42c32ce95f0571304e233c37
Julien Desrosiers <juliend2@gmail.com>
2025-09-15 16:51:41 -0400

    Display content

--- apparitions/index.php
+++ apparitions/index.php
@@ -2,54 +2,50 @@
 
 $pdo = new PDO("sqlite:" . __DIR__ . "/../data/data.sqlite3");
 
-// Example: read all rows
-$stmt = $pdo->query("SELECT id, name FROM events");
-
 
 include __DIR__ . '/../includes/header.php';
 ?>
 
-<!-- Preview -->
-      <section id="preview">
-        <h2>Preview</h2>
-        <p>
-          Sed ultricies dolor non ante vulputate hendrerit. Vivamus sit amet suscipit sapien. Nulla
-          iaculis eros a elit pharetra egestas.
-        </p>
-        <form>
-          <div class="grid">
-            <input
-              type="text"
-              name="firstname"
-              placeholder="First name"
-              aria-label="First name"
-              required
-            >
-            <input
-              type="email"
-              name="email"
-              placeholder="Email address"
-              aria-label="Email address"
-              autocomplete="email"
-              required
-            >
-            <button type="submit">Subscribe</button>
-          </div>
-          <fieldset>
-            <label for="terms">
-              <input type="checkbox" role="switch" id="terms" name="terms">
-              I agree to the
-              <a href="#" onclick="event.preventDefault()">Privacy Policy</a>
-            </label>
-          </fieldset>
-        </form>
-      </section>
+<?php if (isset($_GET['id'])): ?>
 
 
-<?php foreach ($stmt as $row): ?>
-    <a href="?id=<?= $row['id'] ?><?= $row['name'] ?></a>
-<?php endforeach ?>
+  <?php
+  $sql = "SELECT id, name, description FROM events WHERE id = :id";
+  $stmt = $pdo->prepare($sql);
 
+  // Bind the value safely
+  $stmt->execute([':id' => 1]);
+
+  // Fetch a single row
+  $event = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if ($event) {
+    ?>
+    <h1><?= $event['name'] ?></h1>
+    <p>
+      <?= nl2br($event['description']) ?>
+    </p>
+    <?php
+  } else {
+      echo "Mauvaise adresse, désolé.";
+  }
+  ?>
+
+<?php else: ?>
+  <?php
+
+  $stmt = $pdo->query("SELECT id, name FROM events");
+
+  ?>
+  <ul>
+  <?php foreach ($stmt as $row): ?>
+    <li>
+      <a href="?id=<?= $row['id'] ?>"><?= $row['name'] ?></a>
+    </li>
+  <?php endforeach ?>
+  </ul>
+
+<?php endif ?>
 
 <?php 
 include __DIR__ . '/../includes/footer.php';
